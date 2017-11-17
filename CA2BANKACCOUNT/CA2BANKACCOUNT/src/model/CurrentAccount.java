1b05b967e82cb2e3c52f37bb8fde663611744c33
Grizzly232 <michael.brady.ie@gmail.com>
2017-11-17 14:00:02 +0000

    Add files via upload

---
+++ CA2BANKACCOUNT/CA2BANKACCOUNT/src/model/CurrentAccount.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author x00136708
+ */
+public class CurrentAccount extends BankAccount {
+    
+    @Override
+    public void withdraw(double amount){
+        
+    }
+    
+}
